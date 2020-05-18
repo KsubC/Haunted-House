8cbb0ac6dcf52fe844a764012307ff673855cb5c
Casey Young <65471492+KsubC@users.noreply.github.com>
2020-05-18 19:23:06 -0400

    Create Dracula.py

---
+++ Dracula.py
@@ -0,0 +1,42 @@
+## Choose your Adventure interactive Haunted House -- 5 different forks in paths (choosing rooms)
+# solve math problems to defeat monsters
+print("\n")
+print("Please store your name and age. . .")
+print("\n")
+your_name = "kc, your friendly neighborhood equilibrium constant"
+your_age = "over 9000"
+is_female = True  # Boolean value
+print("Hello, there, " + your_name + ". . . ")
+print("\n")
+print("Outside, the world is still, stagnant")
+print("Not even the smallest, most hidden insects dare to break the silence with their chitter")
+print("You have been walking through the woods all night, and happen upon a journal on the forest floor")
+print("There is a deadly chill in the night air")
+print("\n")
+print("You trip on a memory sending you falling backwards through time... to before you were born")
+print("Your stomach turns as you remember a familiar yet unfamiliar voice")
+print("Something, possibly, from another lifetime. . .") # you are Van Helsing, reincarnated
+quote1 = "'Listen to them, the children of the night. What music they make!'" # no double ""
+print(quote1)
+print("The howling all around you is getting louder as you approach Dracula's mansion")
+print("Eerie flames line your path to the door as you spy 50 silvery eyes follow you from the treeline")
+print("You are compelled to enter. . .")
+print("\n")
+print("...")
+print("\n")
+character_name = "Mina Harker" # string variable
+character_age = "50" # you can report this as as an integer, but use string in order to print in string
+is_female = True
+print("Inside the mansion. . .")
+print("\n")
+phrase = "mina harker's journal"
+print(phrase.upper())
+print("My name is " + character_name + ", and I am " + character_age + " years old") # concatenation string1 + string2; cannot concatenate string with integer
+print("I'm looking for my husband, Jonathan.")
+print("He has been missing for some time now.")
+print("My gut is telling me that something lured him back to Transylvania.")
+print("It has been 20 years since Van Helsing passed")
+print("I'm afraid Jonathan and I are the only two left alive who know what happened so long ago")
+print("I'm leaving tonight with to find him.")
+print("\n")
+print("AH AH AH")
